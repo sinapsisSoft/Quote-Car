617f8af5c3e2ab98e0ff51a1c8cf0920bc6fee24
Sinapsis Soft <sinapsis.soft.developer@gmail.com>
2019-10-03 00:08:20 -0500

    new changes

--- php/bo/bo_quote.php
+++ php/bo/bo_quote.php
@@ -144,7 +144,7 @@ class BoQuote
             $con = $this->objConntion->connect();
             $con->query("SET NAMES 'utf8'");
             if ($con != null) {
-                $srtQuery='SELECT QUO.id_line,QUO.id_mp,BOF.name_branch_office,MP.name_mp, LINE.name_line, name_client,mail_client,cellphone_client,model_quote,document_client,code_quote, date_quote FROM wp_quote QUO INNER JOIN wp_city CITY ON QUO.id_city=CITY.id_city INNER JOIN wp_line LINE ON QUO.id_line=LINE.id_line INNER JOIN wp_mp MP ON QUO.id_mp=MP.id_mp INNER JOIN wp_branch_office BOF ON CITY.id_city=BOF.id_city WHERE code_quote="'.$code.'"';
+                $srtQuery='SELECT QUO.date_quote,QUO.id_line,QUO.id_mp,BOF.name_branch_office,MP.name_mp, LINE.name_line, name_client,mail_client,cellphone_client,model_quote,document_client,code_quote, date_quote FROM wp_quote QUO INNER JOIN wp_city CITY ON QUO.id_city=CITY.id_city INNER JOIN wp_line LINE ON QUO.id_line=LINE.id_line INNER JOIN wp_mp MP ON QUO.id_mp=MP.id_mp INNER JOIN wp_branch_office BOF ON CITY.id_city=BOF.id_city WHERE code_quote="'.$code.'"';
                // $srtQuery = 'CALL sp_mp_cost(' . $line . ',' . $mp . ')';
                 if ($result = $con->query($srtQuery)) {
                     while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
--- php/pdf/quote_pdf.php
+++ php/pdf/quote_pdf.php
@@ -19,6 +19,7 @@
         $model=$json[0]['model_quote'];
         $idLine=$json[0]['id_line'];
         $idMp=$json[0]['id_mp'];
+        $date=$json[0]['date_quote'];
         $jsonArticle = json_decode($objBo->selectMP($idLine,$idMp),JSON_FORCE_OBJECT);
     }
     $html="";
@@ -41,9 +42,10 @@
                 border: solid 1px black;
                 margin: auto;
                 text-align: center;
+                border: 1px solid black;
             }
-           
-            tfoot{
+            
+            tfoot {
                 background: black;
                 color: white;
             }
@@ -57,54 +59,129 @@
                 text-align: left;
                 padding: 5px;
             }
+            
+            .img-logo {
+                width: 200px;
+            }
+            
+            .text-logo {
+                color: #00AF50;
+                font-weight: bold;
+                font-size: 30px;
+                text-align: center;
+            }
+            
+            .table-2 {
+                position: absolute;
+                top: 25px;
+                right: 25px;
+            }
+            
+            .table-2 table {
+                text-align: left;
+                margin-left: 30px;
+            }
+            
+            .text-footer {
+                text-align: center;
+                width: 400px;
+                margin: auto;
+                margin-top: auto;
+                margin-top: 60px;
+            }
+            .text-right {
+                text-align: right;
+            }
         </style>
     </head>
     
     <body>
-        <h2>Cotización N° $Quo_consec</h2>
+        <div style='margin-left:50px'>
+            <table>
+                <tr>
+                    <td>
+                        <img class='img-logo' src='logo.png'>
+                    </td>
+                    <td class='text-logo'>
+                        Sistemas Web
+                    </td>
+                </tr>
+            </table>
+            <table>
+                <tr>
+                    <td>
+                        <b>Dirección: Calle 224 # 9 - 60 Piso 2</b>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <b>Ciudad: Bogotá</b>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <b>Teléfono:018000185393</b>
+                    </td>
+                </tr>
+            </table>
+        </div>
+        <div class='table-2'>
+            <h2 class='text-logo'>COTIZACIÓN</h2>
+            <table>
+    
+                <tr>
+                    <td><b>N°: $Quo_consec</b></td>
+    
+                </tr>
+                <tr>
+                    <td><b>Fecha:$date</b></td>
+                </tr>
+                <tr>
+                    <td><b>Concesionario: $branchOffice</b></td>
+                </tr>
+                <tr>
+                    <td><b>Línea: $line</b></td>
+                </tr>
+                <tr>
+                    <td><b>Km: $mp</b></td>
+                </tr>
+                <tr>
+                    <td><b>Modelo: $model</b></td>
+                </tr>
+            </table>
+        </div>
+    
+        <div class='table-3' style='margin-left:50px'>
+    
+            <table>
+                <tr>
+                    <td style='background:#00AF50; font-weight: bold; width: 250px;color: white'>CLIENTE</td>
+                </tr>
+                <tr>
+                    <td><b>Cliente: $name</b></td>
+    
+                </tr>
+                <tr>
+                    <td><b>Correo: $mail</b></td>
+    
+                </tr>
+                <tr>
+                    <td><b>Teléfono: $celPhone</b></td>
+    
+                </tr>
+            </table>
+        </div>
+    
         <hr>
     
-        <table>
-    
-            <tr>
-                <td><b>Cotización N°: $Quo_consec</b></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td><b>Cliente: $name</b></td>
-                <td> </td>
-            </tr>
-            <tr>
-                <td><b>Correo: $mail</b></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td><b>Teléfono: $celPhone</b></td>
-                <td></td>
-            </tr>
-        </table>
-        <hr>
-        <table class='table2'>
-            <tr>
-                <th style='width:300px; background:gray'>Concesionario</th>
-                <th style='width:100px; background:gray'>Linea</th>
-                <th style='width:100px; background:gray'>Km</th>
-                <th style='width:100px; background:gray'>Modelo</th>
-            </tr>
-            <tr>
-                <td>$branchOffice</td>
-                <td>$line</td>
-                <td>$mp</td>
-                <td>$model</td>
-            </tr>
-        </table>
         <table class='table2'>
     
             <tr>
-                <th style='width:500px; background:gray' class='td-3'>Ítems</th>
-                <th></th>
-                <th style='background:gray; width:100px;text-align:center'>Costo</th>
-            </tr    >
+                <th style='width:500px; background:#00AF50; color: white' class='td-3'>DESCRIPCIÓN</th>
+                <th style=' background:#00AF50; color: white' class='td-3'>CANTIDAD</th>
+                <th style=' background:#00AF50'></th>
+                <th style=' width: 50px; background:#00AF50;text-align:center; color: white'>TOTAL</th>
+            </tr>
             <tbody>
             ";
             $items="";
@@ -112,33 +189,38 @@
             foreach($jsonArticle as $article){
                 $sumCost+=$article['cost_article_mp'];
                 $items.="<tr>
-                <td class='td-3'>".$article['name_article']."</td><td>$</td><td class='td-2'>".$article['cost_article_mp']."</td>
+                <td class='td-3'>".$article['name_article']."</td><td>1</td><td>$</td><td class='td-2'>".$article['cost_article_mp']."</td>
             </tr>";
           
             }
             $iva=$sumCost*0.19;  
             $total=$iva+$sumCost;
             $tfoot="</tbody>
-            <tfoot>
+            <tfoot style='margin-top:50px'>
                 <tr>
-                    <td class='text-center' style='background:blue'>IVA 19 %</td>
-                    <td class='text-right' style='background:blue'>$</td>
-                    <td class='td-2'style='background:blue'>$iva</td>
+                    <td colspan='2' class='text-right' style='background:gray;color:white'>Sub Total</td>
+                    <td class='text-right' style='background:gray;color:white'>$</td>
+                    <td class='td-2' style='background:gray;color:white'>$sumCost</td>
                 </tr>
                 <tr>
-                    <td class='text-center' style='background:blue'>Sub Total</td>
-                    <td class='text-right' style='background:blue'>$</td>
-                    <td class='td-2'style='background:blue'>$sumCost</td>
+                    <td colspan='2' class='text-right' style='background:gray;color: white'>IVA 19 %</td>
+                    <td class='text-right' style='background:gray;color:white'>$</td>
+                    <td class='td-2' style='background:gray;color:white'>$iva</td>
                 </tr>
+    
                 <tr>
-                    <td class='text-center' style='background:blue'>Total</td>
-                    <td class='text-right' style='background:blue'>$</td>
-                    <td class='td-2'style='background:blue'>$total</td>
+                    <td colspan='2' class='text-right' style='background:gray;color:white'>Total</td>
+                    <td class='text-right' style='background:gray;color:white'>$</td>
+                    <td class='td-2 ' style='background:gray;color:white'>$total</td>
                 </tr>
             </tfoot>
         </table>
-    </body>
-    </html>";
+    
+        <div class='footer'>
+            <p class='text-footer'>
+                Todos los precios a cliente son sugeridos. Los precios de mano de obra pueden variar según el taller o ciudad Para más información www.autocom.com.co
+            </p>
+        </div>";
     $html=$htmlEstructur.$items.$tfoot;
 ?>
 <?php
